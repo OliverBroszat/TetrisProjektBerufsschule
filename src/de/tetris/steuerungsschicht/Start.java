e870d066d57c0a6780f33d74dcd7b3387aee34a6
Broszat.Oliver <Broszat.Oliver@HERTZ.local>
2016-02-18 13:13:18 +0100

    Ausgabe hinzugefügt

--- src/de/tetris/steuerungsschicht/Start.java
+++ src/de/tetris/steuerungsschicht/Start.java
@@ -3,5 +3,7 @@ public class Start {
 	public static void main(String[] args) {
 		Controller game = new Controller();
 		game.gameLoop();
+		
+		System.out.println("Programm hat gestartet.");
 	}
 }
