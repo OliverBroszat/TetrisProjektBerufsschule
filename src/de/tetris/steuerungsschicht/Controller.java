480825e1ac463602f6d8fafa02592ca2e084b096
Broszat.Oliver <Broszat.Oliver@HERTZ.local>
2016-02-18 14:03:49 +0100

    funktioniert

--- src/de/tetris/steuerungsschicht/Controller.java
+++ src/de/tetris/steuerungsschicht/Controller.java
@@ -6,41 +6,48 @@ import java.util.List;
 import de.tetris.darstellungsschicht.Frame;
 import de.tetris.datenschicht.PersistanceStore;
 
-public class Controller {
+public class Controller implements Runnable {
 	private Thread thread;
 	private boolean gameRunning;
-	
+
 	private Rotator rotator;
 	private XMLSerializer xmlSerializer;
 	private List<Form> formList = new ArrayList<Form>();
 	private Frame frame;
 	private PersistanceStore persistancestore;
-	
-	public Controller() {
-		thread = new Thread();
-		gameRunning = true;
-	}
-	
-	
-	public void gameLoop()
-	{
-	   long lastLoopTime = System.nanoTime();
-	   final int TARGET_FPS = 60;
-	   final long OPTIMAL_TIME = 1000000000 / TARGET_FPS;   
 
-	   // keep looping round until the game ends
-	   while (gameRunning)
-	   {
-	      long now = System.nanoTime() - lastLoopTime;
-	      
-	      if(now >= OPTIMAL_TIME){
-	    	  lastLoopTime = System.nanoTime();
-	    	
-	    	  //TODO mach dat
-	    	  	//render();
-	    	  
-	    	  	//gamelogic();
-	      }
-	   }
+	public Controller() {
+		startGame();
+	}
+
+	public void startGame() {
+		thread = new Thread(this);
+		gameRunning = true;
+		thread.start();
+	}
+
+	public void gameLoop() {
+		long lastLoopTime = System.nanoTime();
+		final int TARGET_FPS = 60;
+		final long OPTIMAL_TIME = 1000000000 / TARGET_FPS;
+
+		// keep looping round until the game ends
+		while (gameRunning) {
+			long now = System.nanoTime() - lastLoopTime;
+
+			if (now >= OPTIMAL_TIME) {
+				lastLoopTime = System.nanoTime();
+
+				// TODO mach dat
+				// render();
+System.out.println("loop");
+				// gamelogic();
+			}
+		}
+	}
+
+	@Override
+	public void run() {
+		gameLoop();
 	}
 }
--- src/de/tetris/steuerungsschicht/Start.java
+++ src/de/tetris/steuerungsschicht/Start.java
@@ -2,8 +2,7 @@ package de.tetris.steuerungsschicht;
 public class Start {
 	public static void main(String[] args) {
 		Controller game = new Controller();
-		game.gameLoop();
-		
+	
 		System.out.println("Programm hat gestartet.");
 	}
 }
