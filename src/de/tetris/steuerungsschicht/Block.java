48a93b0dda86ee103d2118920134b55e69c5581e
Stamm.Marvin <Stamm.Marvin@HERTZ.local>
2016-02-18 14:06:12 +0100

    Blockklasse fertig

--- src/de/tetris/steuerungsschicht/Block.java
+++ src/de/tetris/steuerungsschicht/Block.java
@@ -2,4 +2,44 @@ package de.tetris.steuerungsschicht;
 
 public class Block {
 
+	private Block nachbarOben;
+	private Block nachbarRechts;
+	private Block nachbarUnten;
+	private Block nachbarLinks;
+	
+	/**
+	 * Überlegung ob Farbwahl in Form/Controller anstatt Attribut
+	*/
+	
+	public Block getNachbarOben() {
+		return nachbarOben;
+	}
+	
+	public void setNachbarOben(Block nachbarOben) {
+		this.nachbarOben = nachbarOben;
+	}
+	
+	public Block getNachbarRechts() {
+		return nachbarRechts;
+	}
+	
+	public void setNachbarRechts(Block nachbarRechts) {
+		this.nachbarRechts = nachbarRechts;
+	}
+	
+	public Block getNachbarUnten() {
+		return nachbarUnten;
+	}
+	
+	public void setNachbarUnten(Block nachbarUnten) {
+		this.nachbarUnten = nachbarUnten;
+	}
+	
+	public Block getNachbarLinks() {
+		return nachbarLinks;
+	}
+	
+	public void setNachbarLinks(Block nachbarLinks) {
+		this.nachbarLinks = nachbarLinks;
+	}
 }
