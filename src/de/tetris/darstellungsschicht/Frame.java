af6bdc780f190bf0471b180a6d4f39effbfe5b30
Broszat.Oliver <Broszat.Oliver@HERTZ.local>
2016-02-19 12:21:41 +0100

    Fenster austauschbar, Spielfeld

--- src/de/tetris/darstellungsschicht/Frame.java
+++ src/de/tetris/darstellungsschicht/Frame.java
@@ -1,17 +1,38 @@
 package de.tetris.darstellungsschicht;
 
+import java.awt.CardLayout;
 import java.awt.Color;
 
 public class Frame extends GUI{
 	private static final int HÖHE = 800;
-	private static final int BREITE = HÖHE / 100 * 90; 
+	private static final int BREITE = HÖHE;
+	private CardLayout cardLayout = new CardLayout();
 	
 	public Frame() {
 		super("Tetriges Tetris");
 		setSize(BREITE, HÖHE);
 		setBackground(Color.gray);
+		
+		setLayout(cardLayout);
+		
+		add(addSpielfeld());
+		add(addLoginScreen());
 		setLocationRelativeTo(null);
 		setVisible(true);
 		setResizable(false);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
+	
+	private Panel addSpielfeld() {
+		FrameSpielfeld frameSpielfeld = new FrameSpielfeld();
+		return frameSpielfeld;
+	}
+
+	private Panel addLoginScreen(){
+		Panel tPanel = new Panel();
+		tPanel.setSize(BREITE,HÖHE);
+
+		return tPanel;
+	}
+
 }
--- src/de/tetris/steuerungsschicht/Start.java
+++ src/de/tetris/steuerungsschicht/Start.java
@@ -1,8 +1,13 @@
 package de.tetris.steuerungsschicht;
+
+import de.tetris.darstellungsschicht.Frame;
+
 public class Start {
 	public static void main(String[] args) {
 		Controller game = new Controller();
 	
 		System.out.println("Programm hat gestartet.");
+		
+		new Frame();
 	}
 }
