00cd7cb92c75496a94543ba11f085718196ab2cc
Stamm.Marvin <Stamm.Marvin@HERTZ.local>
2016-02-19 10:15:23 +0100

    message

--- src/de/tetris/steuerungsschicht/Form.java
+++ src/de/tetris/steuerungsschicht/Form.java
@@ -5,17 +5,43 @@ import java.util.List;
 
 public abstract class Form {
 	List blockList = new ArrayList<Block>();
+	private Block startBlock;
+	private int anzahlBloecke;
 	
 	public void formeBlock() {
-		Block  hauptBlock = new Block();
-		int zufall = (int)((Math.random()*4)+1);
+		startBlock = new Block();
+		anzahlBloecke = 0;
+		pruefeNachbar(startBlock);		
 	}
 	
-	public void pruefeNachbar() {
-		
-	}
-	
-	public void waehleRichtung() {
-		
+	private void pruefeNachbar(Block blockRichtung) {
+		if(anzahlBloecke <= 3) {
+			Block block = new Block();
+			int zufall = (int)((Math.random()*4)+1);
+			anzahlBloecke = anzahlBloecke + 1;
+			switch (zufall) {
+			case 1 :	if(blockRichtung.getNachbarRechts() != null) {
+							blockRichtung.setNachbarRechts(block);
+							pruefeNachbar(blockRichtung.getNachbarRechts());
+						}
+						break;
+			case 2 :	if(blockRichtung.getNachbarUnten() != null) {
+							blockRichtung.setNachbarUnten(block);
+							pruefeNachbar(blockRichtung.getNachbarUnten());
+						}
+						break;
+			case 3 :	if(blockRichtung.getNachbarLinks() != null) {
+							blockRichtung.setNachbarLinks(block);
+							pruefeNachbar(blockRichtung.getNachbarLinks());
+						}
+						break;
+			case 4 :	if(blockRichtung.getNachbarOben() != null) {
+							blockRichtung.setNachbarOben(block);
+							pruefeNachbar(blockRichtung.getNachbarOben());
+						}
+						break;
+			default:
+							}
+		}
 	}
 }
