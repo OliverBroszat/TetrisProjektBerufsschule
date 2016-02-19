1518e8dde03ca2f3a79d3b7e445b7c723673d398
Broszat.Oliver <Broszat.Oliver@HERTZ.local>
2016-02-19 08:48:45 +0100

    implemented basic GUI

--- src/de/tetris/darstellungsschicht/Frame.java
+++ src/de/tetris/darstellungsschicht/Frame.java
@@ -1,5 +1,17 @@
 package de.tetris.darstellungsschicht;
 
-public class Frame extends GUI{
+import java.awt.Color;
 
+public class Frame extends GUI{
+	private static final int HÖHE = 800;
+	private static final int BREITE = HÖHE / 100 * 90; 
+	
+	public Frame() {
+		super("Tetriges Tetris");
+		setSize(BREITE, HÖHE);
+		setBackground(Color.gray);
+		setLocationRelativeTo(null);
+		setVisible(true);
+		setResizable(false);
+	}
 }
--- src/de/tetris/darstellungsschicht/GUI.java
+++ src/de/tetris/darstellungsschicht/GUI.java
@@ -2,6 +2,8 @@ package de.tetris.darstellungsschicht;
 
 import javax.swing.JFrame;
 
-public abstract class GUI extends JFrame{
-
+public abstract class GUI extends JFrame {
+	public GUI(String s) {
+		super(s);
+	}
 }
