50df1c2598c0a3cc0119697750b6342d6e28fa3d
Broszat.Oliver <Broszat.Oliver@HERTZ.local>
2016-02-19 13:26:09 +0100

    Menue Screen

--- src/de/tetris/darstellungsschicht/FrameLoginScreen.java
+++ src/de/tetris/darstellungsschicht/FrameLoginScreen.java
@@ -4,27 +4,31 @@ import java.awt.Color;
 import java.awt.Font;
 
 import javax.swing.JButton;
-import javax.swing.JTextField;
+import javax.swing.JLabel;
 
 public class FrameLoginScreen extends Panel{
 
 	private JButton loginButton = new JButton();
 	private JButton highScoreButton = new JButton();
-	private JTextField titelTextField = new JTextField();
+	private JLabel titelLabel = new JLabel("Der Tetris");
 
 	public FrameLoginScreen() {
 	    setLayout(null);
+	    setBackground(Color.gray);
 	    
+	    titelLabel.setFont(new Font("Arial", Font.BOLD, 46));
+	    titelLabel.setForeground(Color.BLUE);
 	    highScoreButton.setFont(new Font("Arial", Font.BOLD, 14));
 	    loginButton.setFont(new Font("Arial", Font.BOLD, 14));
 	    
 	    loginButton.setText("Login");
 	    highScoreButton.setText("Highscore");
 	    
-	    setBackground(Color.gray);
+	    titelLabel.setBounds(295,100,300,70);
 	    highScoreButton.setBounds(260,200,300,70);
 	    loginButton.setBounds(260,300,300,70); 
 
+	    add(titelLabel);
 	    add(highScoreButton);
 	    add(loginButton);
 	}
