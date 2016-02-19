6e42467fc6481dd5000f3ee4fc62ee639932d3f9
Stamm.Marvin <Stamm.Marvin@HERTZ.local>
2016-02-19 09:59:44 +0100

    Form der Blöcke erstellen

--- src/de/tetris/steuerungsschicht/Form.java
+++ src/de/tetris/steuerungsschicht/Form.java
@@ -5,4 +5,17 @@ import java.util.List;
 
 public abstract class Form {
 	List blockList = new ArrayList<Block>();
+	
+	public void formeBlock() {
+		Block  hauptBlock = new Block();
+		int zufall = (int)((Math.random()*4)+1);
+	}
+	
+	public void pruefeNachbar() {
+		
+	}
+	
+	public void waehleRichtung() {
+		
+	}
 }
