1e86b79896b2cecb72cd8c79df4805729017c305
Oliver Broszat <Oliver.Broszat@codecentric.de>
2016-02-22 16:22:21 +0100

    removed unused import

--- src/de/tetris/steuerungsschicht/Start.java
+++ src/de/tetris/steuerungsschicht/Start.java
@@ -1,7 +1,5 @@
 package de.tetris.steuerungsschicht;
 
-import de.tetris.darstellungsschicht.Frame;
-
 public class Start {
 	public static void main(String[] args) {
 		Controller game = new Controller();
