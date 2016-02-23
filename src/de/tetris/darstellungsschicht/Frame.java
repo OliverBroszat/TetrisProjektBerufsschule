45805d8dd9952506f214ccde317563a5812fcb91
Stamm.Marvin <Stamm.Marvin@HERTZ.local>
2016-02-23 10:51:20 +0100

    Action/KeyListener überarbeitet

--- src/de/tetris/darstellungsschicht/Frame.java
+++ src/de/tetris/darstellungsschicht/Frame.java
@@ -6,11 +6,20 @@ import java.awt.Component;
 
 import de.tetris.steuerungsschicht.Controller;
 
+import java.awt.CardLayout;
+import java.awt.Color;
+import java.awt.Component;
+
+import javax.swing.JPanel;
+
+import de.tetris.steuerungsschicht.Controller;
+
 public class Frame extends GUI{
 	private static final int HOEHE = 800;
 	private static final int BREITE = HOEHE;
 	private CardLayout cardLayout = new CardLayout();
 	private Controller controller;
+	private JPanel panel;
 	
 	public Frame(Controller controller) {
 		super("Tetriges Tetris");
@@ -20,25 +29,28 @@ public class Frame extends GUI{
 		setLayout(cardLayout);
 
 		this.controller = controller;
-
-		//	add(addLoginScreen());
+		
+	//	add(addLoginScreen());
 		add(addSpielfeld());
-//		add(addFrameLoginScreen());
+	//	add(addFrameLoginScreen());
 		
 		setLocationRelativeTo(null);
 		setVisible(true);
 		setResizable(false);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
+		controller.spielfedRequestFocus(panel);
 	}
 	
 	private Panel addFrameLoginScreen() {
 		FrameLoginScreen frameLoginScreen = new FrameLoginScreen();
+		controller.createListener(frameLoginScreen);
 		return frameLoginScreen;
 	}
 
 	private Panel addSpielfeld() {
 		FrameSpielfeld frameSpielfeld = new FrameSpielfeld();
+		panel = frameSpielfeld;
 		controller.createListener(frameSpielfeld);
 		return frameSpielfeld;
 	}
